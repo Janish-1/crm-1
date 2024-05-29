050e4b56b3a065de32356e182228dc7bb33d338a
Janish-1 <janish.pancholi11@gmail.com>
2024-05-29 11:48:56 +0530

    added new project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,40 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
-     * Register any application services.
+     * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function register(): void
-    {
-        //
+    public function boot(UrlGenerator $url) {
+
+        //[growcrm] disable debug bar in production mode
+        if (!env('APP_DEBUG_TOOLBAR')) {
+            \Debugbar::disable();
+        }
+
+        //[growcrm] force SSL rul's
+        if (env('ENFORCE_SSL', false)) {
+            $url->forceScheme('https');
+        }
+
+        //[growcrm]
+        $this->app->bind(Carbon::class, function (Container $container) {
+            return new Carbon('now', 'Europe/Brussels');
+        });
+
     }
 
     /**
-     * Bootstrap any application services.
+     * Register any application services.
+     *
+     * @return void
      */
-    public function boot(): void
-    {
+    public function register() {
         //
     }
 }
